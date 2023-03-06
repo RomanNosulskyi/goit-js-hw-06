447c8af3d1cac264ec2afc13bcdd6ec53335d2d1
RomanNosulskyi <nosulskyiroman@gmail.com>
2023-03-06 09:32:50 +0200

    sffss

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,8 @@ const itemEl = listEl.querySelectorAll(".item");
 console.log(`Number of categories: ${itemEl.length}`);
 
 itemEl.forEach((element) => {
-  const itemName = element.querySelector("h2").textContent;
-  const itemElnts = element.querySelectorAll("li").length;
+  const itemName = element.firstElementChild.textContent;
+  const itemElnts = element.lastElementChild.children.length;
   console.log(`Category: ${itemName}`);
   console.log(`Elements: ${itemElnts}`);
 });
--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,16 @@
 const form = document.querySelector("form.login-form");
-const submitBtn = form.querySelector('button[type="submit"]');
+form.addEventListener("submit", onSubmit);
 
-form.addEventListener("submit", function (event) {
+function onSubmit(event) {
   event.preventDefault();
 
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
-
-  if (email === "" || password === "") {
-    alert("Будь-ласка, заповніть всі поля форми");
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Будь-ласка, заповніть всі поля форми");
   } else {
-    const formData = {
-      email,
-      password,
-    };
+    console.log(`Email: ${email.value}, Password ${password.value}`);
+    event.currentTarget.reset();
   }
-});
-console.log(formData);
+}
